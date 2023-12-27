fbca576774128055281f25ae3ba8a855a530c907
Rohitraj Khorwal <90453079+Rohitrajkhorwal@users.noreply.github.com>
2023-12-27 17:55:45 +0530

    27 Dec 2023
    app: source code added.

---
+++ app/src/main/java/com/mtma/insta/downloader/MyApplication.java
@@ -0,0 +1,15 @@
+package com.mtma.insta.downloader;
+
+
+import android.app.Application;
+
+
+public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+
+}
